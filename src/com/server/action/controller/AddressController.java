22e2334801432a850c1487b20ebfa269a6f18a8d
taolichao <xvhandong@sina.com>
2016-03-16 16:15:17 +0800

    3

--- src/com/server/action/controller/AddressController.java
+++ src/com/server/action/controller/AddressController.java
@@ -2,10 +2,6 @@ package com.server.action.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
